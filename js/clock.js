13517773bf45933d1f4f2cb9d1e99419ea78716f
JSG <72681851+amkso@users.noreply.github.com>
2024-07-17 09:52:10 +0900

    Update

--- js/clock.js
+++ js/clock.js
@@ -2,15 +2,14 @@ const clock = document.querySelector("h2#clock");
 const greeting = document.querySelector("#greeting");
 
 function getClock() {
-    // const date = new Date();
-    // const hours = String(date.getHours()).padStart(2, "0");
-    // const minutes = String(date.getMinutes()).padStart(2, "0");
-    // const seconds = String(date.getSeconds()).padStart(2, "0");
-    // clock.innerText = `${hours}:${minutes}:${seconds}`;
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}`;
 
     // another way
-    const date2 = new Date();
-    clock.innerText = date2.toLocaleTimeString("en-US",{hour12: false});
+    // const date2 = new Date();
+    // clock.innerText = date2.toLocaleTimeString("en-US",{hour12: false});
 }
 
 getClock();
